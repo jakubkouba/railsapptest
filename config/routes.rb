04b34a2b6c2bb53856e568a0049df994b8dd6334
Jakub Adler <adler.jakub@gmail.com>
2023-03-07 11:01:39 +0000

    feat: add root route "hello world"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'hello#index'
+  
   get 'health_check', to: 'health_check#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
