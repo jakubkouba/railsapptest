cf226c90becb78588dd9e3ff2321000a6c460280
Jakub Adler <adler.jakub@gmail.com>
2023-03-07 11:18:50 +0000

    feat: add system info to root route

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,5 +1,28 @@
 class HelloController < ApplicationController
   def index
-    render json: { message: 'Hello World' }
+    render json: system_info
+  end
+
+  private
+
+  def system_info
+    user = `whoami`.strip
+    ruby_version = `ruby --version`.strip
+    host_name = `scutil --get ComputerName`.strip
+    ip = `ifconfig en0 | grep netmask`.split.second
+    os = `sw_vers | grep ProductName`.split.last
+    version = (`sw_vers | grep ProductVersion`).split.last
+
+    @system_info ||= {
+      status: 'OK',
+      system_info: {
+        user: user,
+        ruby_version: ruby_version,
+        host_name: host_name,
+        ip: ip,
+        os: os,
+        version: version
+      }
+    }
   end
 end
